ac1c28da9948fa6e6b6027c501162fa78ddfcea6
annglu123 <anna1991leo@gmail.com>
2025-04-20 20:36:43 +0300

    Add files via upload

---
+++ lab1/task4.py
@@ -0,0 +1,9 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+dict_1 = {"Общее количество": 0 , "Уникальные посещения": 0}
+dict_1["Общее количество"] = len(users)
+dict_1["Уникальные посещения"] = len(set(users))
+print(dict_1)
+
+#users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+#dict_1 = {"Общее количество": len(users) , "Уникальные посещения": len(set(users))}
+#print(dict_1)
